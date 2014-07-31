be2f05ac816acc8a0fb56058139e465b3916a13b
Ben Plumley <benplumley@gmail.com>
2014-07-31 21:34:56 +0100

    Initial push of all files onto GitHub

---
+++ TotallyDaemon/TotallyDaemon/Form1.vb
@@ -0,0 +1,133 @@
+﻿Imports System.IO
+Imports SQLite
+Imports System.Data.SQLite
+Imports System.Environment
+
+Public Class Form1
+
+    'Text file syntax:
+    'Date Time
+    'Expense:1:Amount:2:Shop:3:Description:4:Category:5:Layer:6:
+
+    Public databaseAddress As String = GetFolderPath(SpecialFolder.ApplicationData) & "\Totally\Budget.db"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Threading.Thread.Sleep(60 * 1000) 'Sleep for one minute to allow dropbox to sync
+
+
+
+        Dim base64Path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Dropbox\host.db") 'Gets the location of the dropbox folder
+        Dim lines As String() = System.IO.File.ReadAllLines(base64Path)
+        Dim dbBase64Text As Byte() = Convert.FromBase64String(lines(1))
+        Dim dropboxBasePath As String = System.Text.ASCIIEncoding.ASCII.GetString(dbBase64Text) & "\Totally\New\" 'Turns the path into a string
+        Dim dropboxPath As String = dropboxBasePath
+
+        Dim counter = My.Computer.FileSystem.GetFiles(dropboxBasePath)
+        Dim numberOfFilesToAdd As Integer = counter.Count - 1 'The number of files that have been added via mobile since last run
+
+        Dim expense As String
+        Dim amount As Single
+        Dim shop As String
+        Dim description As String
+        Dim category As String
+        Dim layer As Integer
+
+        Dim dDate As String
+        Dim month As String
+        Dim day As String
+        Dim year As String
+        Dim time As String
+        For i = 1 To numberOfFilesToAdd
+            dropboxPath = dropboxBasePath & "file (" & i & ").txt"
+            Dim textFileReader As New StreamReader(dropboxPath)
+            Dim firstLine As String = textFileReader.ReadLine
+            Try
+                Select Case Mid(firstLine, 1, InStr(firstLine, " ") - 1) 'Takes the first word of the first line and turns it from a month word to a month number
+                    Case "January"
+                        month = "01"
+                    Case "February"
+                        month = "02"
+                    Case "March"
+                        month = "03"
+                    Case "April"
+                        month = "04"
+                    Case "May"
+                        month = "05"
+                    Case "June"
+                        month = "06"
+                    Case "July"
+                        month = "07"
+                    Case "August"
+                        month = "08"
+                    Case "September"
+                        month = "09"
+                    Case "October"
+                        month = "10"
+                    Case "November"
+                        month = "11"
+                    Case "December"
+                        month = "12"
+                    Case Else
+                        MsgBox("Parsing error adding Dropbox/Totally/New/file (" & i & ").txt to the Totally budget database. Open the file and check the format, then save and close. If this error happens again, add the data manually in the Totally application then delete the text file. If this error persists between text files, make sure they are the correct format which is specified in Dropbox/Budget/New/file.txt.")
+                End Select
+            Catch ex As Exception
+                MsgBox("Parsing error adding Dropbox/Totally/New/file (" & i & ").txt to the Totally budget database. Open the file and check the format, then save and close. If this error happens again, add the data manually in the Totally application then delete the text file. If this error persists between text files, make sure they are the correct format which is specified in Dropbox/Budget/New/file.txt.")
+            End Try
+            day = Mid(firstLine, InStr(firstLine, " ") + 1, 2)
+            year = Mid(firstLine, InStr(firstLine, ", ") + 2, 4)
+            dDate = day & "/" & month & "/" & year
+            Select Case Mid(firstLine, InStr(firstLine, "at ") + 8, 2)
+                Case "AM"
+                    time = Mid(firstLine, InStr(firstLine, "at " + 3), 4)
+                Case "PM"
+                    time = Mid(firstLine, InStr(firstLine, "at ") + 3, 5)
+                    Dim hours As Integer = CInt(Mid(time, 1, 2))
+                    hours += 12
+                    time = hours & (Mid(time, 3, 3))
+                Case Else
+                    MsgBox("Parsing error adding Dropbox/Totally/New/file (" & i & ").txt to the Totally budget database. Open the file and check the format, then save and close. If this error happens again, add the data manually in the Totally application then delete the text file. If this error persists between text files, make sure they are the correct format which is specified in Dropbox/Budget/New/file.txt.")
+            End Select
+            Try
+                expense = textFileReader.ReadLine
+
+                textFileReader.ReadLine()
+                Dim amountString As String = textFileReader.ReadLine
+                If Mid(amountString, 1, 1) = "£" Then 'If they've included the pound sign
+                    amount = Mid(amountString, 2)
+                Else
+                    amount = amountString
+                End If
+                textFileReader.ReadLine()
+                shop = textFileReader.ReadLine
+                textFileReader.ReadLine()
+                description = textFileReader.ReadLine
+                textFileReader.ReadLine()
+                category = textFileReader.ReadLine
+                textFileReader.ReadLine()
+                layer = 1
+
+                writeToDatabase(expense, amount, shop, description, dDate, time, category, layer)
+                textFileReader.Close()
+                My.Computer.FileSystem.DeleteFile(dropboxPath)
+            Catch ex As Exception
+                MsgBox("Parsing error adding Dropbox/Totally/New/file (" & i & ").txt to the Totally budget database. Open the file and check the format, then save and close. If this error happens again, add the data manually in the Totally application then delete the text file. If this error persists between text files, make sure they are the correct format which is specified in Dropbox/Budget/New/file.txt.")
+            End Try
+
+
+        Next
+    End Sub
+
+    Sub writeToDatabase(expense, amount, shop, description, dDate, time, category, layer)
+        Using conn As New SQLiteConnection("Data Source=" & databaseAddress)
+            conn.Open()
+            Using cmd As New SQLiteCommand(conn)
+                cmd.CommandText = "INSERT INTO transactions(expense, amount, shop, description, dDate, tTime, category, layer) values(1, " & amount & ", '" & shop & "', '" & description & "', '" & dDate & "', '" & time & "', '" & category & "', " & layer & ")"
+                cmd.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        Me.Close() 'Closes the form as soon as all its load events finish
+    End Sub
+End Class
---
+++ TotallyInstaller/TotallyInstaller/Module1.vb
@@ -0,0 +1,175 @@
+﻿Imports System.Environment
+Imports System.IO
+
+'The following files need to be in the same directory as this installer:
+'Budget.db
+'file.txt
+'TotallyDaemon.exe
+'Totally.exe
+
+Module Module1
+
+    Sub Main()
+        createAppdataFiles()
+        createStartupFiles()
+        createDropboxFiles()
+        runSetup()
+        finished()
+    End Sub
+
+    Sub createAppdataFiles()
+        Dim appdataPath As String = GetFolderPath(SpecialFolder.ApplicationData)
+        Console.WriteLine("Creating appdata folder...")
+        If Not Directory.Exists(appdataPath & "\Totally") Then
+            Directory.CreateDirectory(appdataPath & "\Totally")
+            Console.WriteLine("Appdata folder created successfully.")
+        Else
+            Console.WriteLine("Appdata folder already exists. Continuing...")
+        End If
+
+        Console.WriteLine("Copying blank database to appdata folder...") '### Budget
+        If Not My.Computer.FileSystem.FileExists(appdataPath & "\Totally\Budget.db") Then
+            Try
+                File.Copy(My.Application.Info.DirectoryPath & "\Example Data.db", appdataPath & "\Totally\Budget.db")
+                Console.WriteLine("Database copied successfully.")
+            Catch ex As Exception
+                fileNotFoundException(ex)
+            End Try
+        Else
+            Console.WriteLine("Database already exists. Continuing...")
+        End If
+
+        Console.WriteLine("Copying files to Program Files folder...") '### Totally
+        Try
+            File.Copy(My.Application.Info.DirectoryPath & "\Totally.exe", appdataPath & "\Totally\Totally.exe", True)
+            Console.WriteLine("Files copied successfully.")
+        Catch ex As Exception
+            fileNotFoundException(ex)
+        End Try
+
+        If Not My.Computer.FileSystem.FileExists(appdataPath & "\Totally\blank database template.db") Then '### Budget Template
+            Try
+                File.Copy(My.Application.Info.DirectoryPath & "\Budget.db", appdataPath & "\Totally\blank database template.db")
+                Console.WriteLine("Files copied successfully.")
+            Catch ex As Exception
+                fileNotFoundException(ex)
+            End Try
+        Else
+            Console.WriteLine("Files already exist. Continuing...")
+        End If
+
+        If Not My.Computer.FileSystem.FileExists(appdataPath & "\Totally\file.txt") Then '### Dropbox Placeholder
+            Try
+                File.Copy(My.Application.Info.DirectoryPath & "\file.txt", appdataPath & "\Totally\file.txt")
+                Console.WriteLine("Files copied successfully.")
+            Catch ex As Exception
+                fileNotFoundException(ex)
+            End Try
+        Else
+            Console.WriteLine("Files already exist. Continuing...")
+        End If
+
+        Try
+            File.Copy(My.Application.Info.DirectoryPath & "\TotallyDaemon.exe", appdataPath & "\Totally\TotallyDaemon.exe", True)
+            Console.WriteLine("Files copied successfully.")
+        Catch ex As Exception
+            fileNotFoundException(ex)
+        End Try
+    End Sub
+
+    Sub createDropboxFiles()
+        Try
+            Dim base64Path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Dropbox\host.db")
+            Dim lines As String() = System.IO.File.ReadAllLines(base64Path)
+            Dim dbBase64Text As Byte() = Convert.FromBase64String(lines(1))
+            Dim dropboxPath As String = System.Text.ASCIIEncoding.ASCII.GetString(dbBase64Text)
+
+            Console.WriteLine("Creating dropbox folder...")
+            If Not Directory.Exists(dropboxPath & "\Totally\New") Then
+                Directory.CreateDirectory(dropboxPath & "\Totally\New")
+                Console.WriteLine("Dropbox folder created successfully.")
+            Else
+                Console.WriteLine("Dropbox folder already exists. Continuing...")
+            End If
+
+            Console.WriteLine("Copying files to dropbox folder...")
+            If Not My.Computer.FileSystem.FileExists(dropboxPath & "\Totally\New\file.txt") Then
+                Try
+                    File.Copy(My.Application.Info.DirectoryPath & "\file.txt", dropboxPath & "\Totally\New\file.txt")
+                    Console.WriteLine("Files copied successfully.")
+                Catch ex As Exception
+                    fileNotFoundException(ex)
+                End Try
+            Else
+                Console.WriteLine("Files already exist. Continuing...")
+            End If
+        Catch ex As Exception
+            dropboxNotInstalledException()
+        End Try
+
+    End Sub
+
+    Sub createStartupFiles()
+        Console.WriteLine("Copying files to startup folder...")
+        Dim startupPath As String = Environment.GetFolderPath(Environment.SpecialFolder.Startup)
+        Try
+            File.Copy(My.Application.Info.DirectoryPath & "\TotallyDaemon.exe", startupPath & "\TotallyDaemon.exe", True)
+            Console.WriteLine("Files copied successfully.")
+        Catch ex As Exception
+            fileNotFoundException(ex)
+        End Try
+    End Sub
+
+    Sub runSetup()
+        Try
+            Process.Start(My.Application.Info.DirectoryPath & "\setup.exe")
+        Catch ex As Exception
+            fileNotFoundException(ex)
+        End Try
+    End Sub
+
+    Sub finished()
+        Console.WriteLine()
+        Console.ForegroundColor = ConsoleColor.Black
+        Console.BackgroundColor = ConsoleColor.White
+        Console.WriteLine("Installation has completed successfully. Totally will now launch.")
+        Threading.Thread.Sleep(2000)
+        Console.Clear()
+        Console.WriteLine("Thanks for using Totally!")
+        Threading.Thread.Sleep(1000)
+    End Sub
+
+    Sub fileNotFoundException(ex As Exception)
+        Console.BackgroundColor = ConsoleColor.Red
+        Console.WriteLine("An error occurred. Extract the files from the zip archive again and run the installer without moving or renaming any files in the extracted directory.")
+        Console.BackgroundColor = ConsoleColor.Black
+        Console.WriteLine("Error details: " & ex.Message)
+        Console.WriteLine("Press any key to close this installer and fix the error before trying again.")
+        Console.ReadKey()
+        End
+    End Sub
+
+    Sub dropboxNotInstalledException()
+        Console.BackgroundColor = ConsoleColor.Red
+        Console.WriteLine("An error occurred. Dropbox does not appear to be installed on this computer. Please install Dropbox and then run the installer again.")
+        Console.BackgroundColor = ConsoleColor.Black
+        Console.WriteLine("You can override this error if you are sure that Dropbox is installed. Would you like to? (Y/N)")
+        Dim response As String = UCase(Console.ReadLine)
+        Select Case response
+            Case "Y"
+                Console.WriteLine("Navigate to your Dropbox folder, usually located at C:\Users\%USERNAME%\Dropbox.")
+                Console.WriteLine("Create a new folder called Totally and navigate inside this new folder.")
+                Console.WriteLine("Create a new folder called New and navigate inside this new folder.")
+                Console.WriteLine("In a separate instance of Windows Explorer, navigate to C:\Program Files\Totally or C:\Program Files (x86)\Totally.")
+                Console.WriteLine("Copy the text file called file.txt from Program Files to C:\Users\%USERNAME%\Dropbox\Totally\New.")
+                Console.WriteLine("When this is complete, press any key to return to installation.")
+                Console.ReadKey()
+            Case Else
+                Console.WriteLine("Press any key to close this installer and fix the error before trying again.")
+                Console.ReadKey()
+                End
+        End Select
+    End Sub
+
+
+End Module
